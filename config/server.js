39391d00acc62e20ce0f5ca930cffb8053ee9b07
Edgardo Medina <edgardojosemedina2@gmail.com>
2021-05-28 03:13:03 -0400

    backend push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c21b174d8dc72b98658c8189bc3f9538'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be080a55-76dd-4651-8462-f105679af4fa'
+};
