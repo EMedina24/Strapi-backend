510dbd9f406a0540c58c32983d501d4de09893d6
Edgardo Medina <edgardojosemedina2@gmail.com>
2021-05-31 23:06:05 -0400

    changes to database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,11 +8,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: 'ec2-3-218-71-191.compute-1.amazonaws.com',
+        port: '5432',
+        database: 'd2pdv6ptunpavc',
+        username: 'etzngtegkcpavm',
+        password: '752731f363de6b26321b0c8c42cc3f1a0ca49677c1af678c8935b9c2cc86670e',
         ssl: {
           rejectUnauthorized: false,
         },
